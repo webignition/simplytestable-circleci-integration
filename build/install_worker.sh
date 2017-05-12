17cd1bbed69c8f7d1e0fc702611d38e4f567d6dd
Jon Cram <jon@webignition.net>
2017-05-12 17:34:18 +0100

    Pass worker name as arg to installer

--- build/install_worker.sh
+++ build/install_worker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-git clone git@github.com:webignition/worker.simplytestable.com.git
-cp config/worker.simplytestable.com/app_parameters.yml worker.simplytestable.com/app/config/parameters.yml
-cp worker.simplytestable.com/app/config/security_credentials.yml.dist worker.simplytestable.com/app/config/security_credentials.yml
-cat config/worker.simplytestable.com/bundle_parameters.yml config/worker.simplytestable.com/bundle_parameters.hydrogen > worker.simplytestable.com/src/SimplyTestable/WorkerBundle/Resources/config/parameters.yml
-cd worker.simplytestable.com && composer install
+git clone git@github.com:webignition/worker.simplytestable.com.git $1.worker
+cp config/worker.simplytestable.com/app_parameters.yml $1.worker/app/config/parameters.yml
+cp $1.worker/app/config/security_credentials.yml.dist $1.worker/app/config/security_credentials.yml
+cat config/worker.simplytestable.com/bundle_parameters.yml config/worker.simplytestable.com/bundle_parameters.hydrogen > $1.worker/src/SimplyTestable/WorkerBundle/Resources/config/parameters.yml
+cd $1.worker && composer install

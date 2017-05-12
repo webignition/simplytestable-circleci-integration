6c299a71a2274f889f609dd796e193f42e652b8f
Jon Cram <jon@webignition.net>
2017-05-12 17:55:10 +0100

    cd the right way

--- build/worker_prepare_db.sh
+++ build/worker_prepare_db.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-cd $1.worker && php app/console doctrine:database:create
-cd $1.worker && php app/console doctrine:migrations:migrate --no-interaction
-cd $1.worker && php app/console doctrine:fixtures:load --no-interaction
+cd $1.worker
+php app/console doctrine:database:create
+php app/console doctrine:migrations:migrate --no-interaction
+php app/console doctrine:fixtures:load --no-interaction

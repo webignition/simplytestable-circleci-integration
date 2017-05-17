18b4c154e29ff5134fa7cfe26388539ed178c65a
Jon Cram <jon@webignition.net>
2017-05-17 12:29:49 +0100

    install html5 validator (?)

--- build/html_validator_install.sh
+++ build/html_validator_install.sh
@@ -3,3 +3,9 @@
 sudo mkdir /etc/apache2/conf.d
 sudo apt-get update
 sudo apt-get install w3c-markup-validator
+sudo sed -i 's/Allow Private IPs = no/Allow Private IPs = yes/' /etc/w3c/validator.conf
+
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+git clone https://github.com/validator/validator.git html-validator
+cd html-validator
+python ./build/build.py jar
